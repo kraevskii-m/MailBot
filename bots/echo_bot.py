f758c28d61250095430eb981d3dd0315f9005193
kraevskii-m <kramihm@gmail.com>
2019-05-14 18:40:40 +0300

    minor changes

--- bots/echo_bot.py
+++ bots/echo_bot.py
@@ -5,6 +5,6 @@ address = "echobot-mailbot@yandex.ru"
 
 bot = bot_lib.Bot(TOKEN)
 while True:
-    messages = bot.get_updates
+    messages = bot.get_updates()
     for message in messages:
         bot.send_message(bot_lib.Message(address, message.fr, message.subj, message.body))
